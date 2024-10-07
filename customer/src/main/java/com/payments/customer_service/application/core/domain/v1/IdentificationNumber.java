dbd14a7f813e32cb0ddf48bf159919b0ba7f1332
paulomarchon <paulomarchon@gmail.com>
2024-10-07 18:49:06 -0300

    feat: Adding a default method for identifying the type of Identification Number

--- customer/src/main/java/com/payments/customer_service/application/core/domain/v1/IdentificationNumber.java
+++ customer/src/main/java/com/payments/customer_service/application/core/domain/v1/IdentificationNumber.java
@@ -5,4 +5,13 @@ public interface IdentificationNumber {
     String getNumber();
 
     boolean isValid(String number);
+
+    default IdentificationNumber identificationType(String number) {
+
+        return switch (number.length()) {
+            case 11 -> new Cpf(number);
+            case 14 -> new Cnpj(number);
+            default -> throw new NumberFormatException();
+        };
+    }
 }
